8ae7c67a51520d16df045bf9217b1775f215692d
Kastus <kickoman@yandex.by>
2015-12-13 23:53:39 +0300

    fixed, now it works

--- different/Segment_tree_by_arthur/main.cpp
+++ different/Segment_tree_by_arthur/main.cpp
@@ -10,41 +10,44 @@ vector<int> t;
 int query(int l, int r)
 {
     int res = 0;
+    cerr << l << " " << r << endl;
     for (l += n, r += n; l < r; l >>= 1, r >>= 1)
     {
+        cerr << l << " " << r << " " << res << "\n";
         if (l & 1)
             res += t[l++];
         if (r & 1)
-            res += t[++r];
+            res += t[--r];
     }
     return res;
 }
 
 void build(vector<int> &v)
 {
-    for (int i = 0; i < v.size(); ++i)
+    for (int i = 0; i < n; ++i)
         t[n + i] = v[i];
     for (int i = n - 1; i > 0; --i)
-        t[i] = t[i << 1] + t[i << 1 | 1];
+        t[i] = t[i << 1] + t[i << 1 ^ 1];
 }
 
 int main()
 {
     freopen("task.in", "r", stdin);
     freopen("task.out", "w", stdout);
-    int n;
-    scanf("%d\n", &n);
+    scanf("%d", &n);
     t.resize(2 * n);
     vector<int> a(n + 2);
     for (int i = 0; i < n; ++i)
         scanf("%d", &a[i]);
     build(a);
+
+
     int m;
-    scanf("%d\n", &m);
+    scanf("%d", &m);
     for (int i = 0; i < m; ++i)
     {
         int l, r;
-        scanf("%d %d\n", &l, &r);
+        scanf("%d %d", &l, &r);
         printf("Sum from %d to %d equals %d\n", l, r, query(l - 1, r));
     }
     return 0;
