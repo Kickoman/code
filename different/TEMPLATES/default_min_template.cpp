64b837fe6cbbde1398fa18022f31363bc8ddfab2
Kastus <kickoman@yandex.by>
2015-12-19 00:22:22 +0300

    Templates initialized & first template has been added

---
+++ different/TEMPLATES/default_min_template.cpp
@@ -0,0 +1,31 @@
+/**
+  *     gównokod by candelabra
+***/
+
+#include <iostream>
+#include <cstdio>
+
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define pob pop_back
+
+using namespace std;
+
+#ifdef LOCAL
+    #define TASK "task"
+    #define debug(...) printf(__VA_ARGS__)
+#else
+    #define TASK "lel"
+    #define debug(...)
+    #define cerr if(0)cerr
+#endif // LOCAL
+
+int main()
+{
+    freopen(TASK ".in", "r", stdin);
+    freopen(TASK ".out", "w", stdout);
+
+    return 0;
+}
