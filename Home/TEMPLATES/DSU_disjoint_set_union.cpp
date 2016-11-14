3ac7bbe686b51c28e49109295577e11a2e656e25
Kastus <kickoman@yandex.by>
2016-11-14 22:29:49 +0300

    DSU have been added

---
+++ Home/TEMPLATES/DSU_disjoint_set_union.cpp
@@ -0,0 +1,40 @@
+/* 
+    Systema niaźvazanych mnostvaŭ
+    Kod Artura z majim codestylam
+*/
+
+class dsu
+{
+public:
+    int n;  // Pamier masiva
+    vector<int> t;
+    vector<int> r;
+
+    int get(int x) 
+    {
+        return t[x] == x ? x : t[x] = get(t[x]);
+    }
+
+    int unite(int x, int y) 
+    {
+        x = get(x);
+        y = get(y);
+        if (x == y) 
+            return 0;
+        if (r[x] <= r[y]) 
+        {
+            t[x] = y;
+            r[y] += r[x] == r[y];
+        } else {
+            t[y] = x;
+        }
+        return 1;
+    }
+
+    dsu(int n):n(n),t()
+    {
+        t.resize(n);
+        r.assign(n, 0);
+        for (int i = 0; i < n; i++) t[i] = i;
+    }
+};
