06f9a8d9353e288fe327b43c4a137fa4e7443efc
Kastus <kickoman@yandex.by>
2016-11-14 22:35:43 +0300

    template updated

--- Home/TEMPLATES/default_min_template.cpp
+++ Home/TEMPLATES/default_min_template.cpp
@@ -20,8 +20,7 @@
 ────█░░░░░░░░░░░░░░░░░░░░░░░░█────────
 ──────────────────────────────────────*/
 
-#include <iostream>
-#include <cstdio>
+#include <bits/stdc++.h>
 
 #define F first
 #define S second
@@ -40,11 +39,91 @@ using namespace std;
     #define cerr if(0)cerr
 #endif // LOCAL
 
+typedef long long ll;
+typedef long double ld;
+
+const int INF = int(1e9);
+const ll INFll = ll(1e18);
+const char array_sep[] = " ";
+const char array_end[] = "";
+
+
+const char pair_sep[] = ",";
+const char pair_end[] = "";
+
+
+const char map_sep[] = "->";
+const char map_end[] = "\n";
+
+
+template<typename A> ostream & operator<<(ostream & os, const vector<A> & x)
+{
+    for(int i = 0; i < x.size(); i++)
+      os << x[i] << array_sep;
+    os << array_end;
+    return os;
+}
+
+
+template<typename A, typename B> ostream & operator<<(ostream & os, const pair<A, B> & x)
+{
+    os << x.first << pair_sep << x.second << pair_end;
+    return os;
+}
+
+
+template<typename A> istream & operator>>(istream & is, vector<A> & x)
+{
+    for(int i = 0; i < x.size(); i++)
+      is >> x[i];
+    return is;
+}
+
+
+template<typename A, typename B> istream & operator>>(istream & is, pair<A, B> & x)
+{
+    is >> x.first >> x.second;
+    return is;
+}
+
+
+template<typename _key, typename _val> ostream & operator<<(ostream & os, map<_key, _val> & mp)
+{
+    for(auto it : mp)   // not for C++98 or earlier
+      os << it->F << map_sep << it->S << map_end;
+    return os;
+}
+
+template<typename _key, typename _val> ostream & operator<<(ostream & os, unordered_map<_key, _val> & mp)
+{
+    for(auto it : mp)   // not for C++98 or earlier
+      os << it->F << map_sep << it->S << map_end;
+    return os;
+}
+
+template<typename _key> ostream & operator<<(ostream& os, set<_key>& st)
+{
+    os << "{";
+    for(auto& it: st)
+        os << it << ", ";
+    os << "}";
+}
+
+
+template<typename _response> void die(_response ans)
+{
+    cout << ans << endl;
+    exit(0);
+}
+
 int main()
 {
 //#ifdef LOCAL
     freopen(TASK ".in", "r", stdin);
     freopen(TASK ".out", "w", stdout);
+// #else
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
 //#endif // LOCAL
 
     return 0;
