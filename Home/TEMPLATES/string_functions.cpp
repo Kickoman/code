b267c07300f3950d54991700314743f416ad664d
Kastuś Novikaŭ <kickoman@yandex.by>
2017-02-07 13:25:09 +0300

    improved string functions

--- Home/TEMPLATES/string_functions.cpp
+++ Home/TEMPLATES/string_functions.cpp
@@ -1,6 +1,6 @@
 vector<int> prefix_function(string s)
 {
-    int n = int(s.length());
+    int n = int(s.size());
     vector<int> pi(n);
     for (int i = 1; i < n; ++i)
     {
@@ -12,6 +12,22 @@ vector<int> prefix_function(string s)
         pi[i] = j;
     }
     return pi;
+}
+
+vector<int> z_function(string s) 
+{
+    int n = (int) s.size();
+    vector<int> z (n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) 
+    {
+        if (i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if (i + z[i] - 1 > r)
+            l = i,  r = i + z[i] - 1;
+    }
+    return z;
 }
 
 vector<int> z_to_pref(vector<int> &Z)
