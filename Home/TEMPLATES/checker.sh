43c579b76a720c2757fb10a628b79690bfe3efe8
Kastuś Novikaŭ <kickoman@yandex.by>
2017-03-24 23:30:23 +0300

    Checker for unix
    Comments for checker

--- Home/TEMPLATES/checker.sh
+++ Home/TEMPLATES/checker.sh
@@ -2,11 +2,14 @@
 
 set -e
 
+# To build your solution before testing:
 #g++ -O2 -DLOCAL -std=c++11 main.cpp -o main
+
+
 while [[ true ]]; do
-  ./gen
-  ./itmo
-  python3 kek.py
-  ./check
+  ./gen # Run generator
+  ./solution # Run your solution
+  python3 kek.py # Run brute or true solution
+  ./check # Run checker (return not zero if answers are different)
   #echo "ok";
 done
